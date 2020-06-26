0a88e840fcf34b57ac83cf423bc05ea5b8a70338
Kerry Buckley <kerryjbuckley@gmail.com>
2020-06-26 10:39:56 +0100

    Add failing tests

--- lib/censored_strings.ex
+++ lib/censored_strings.ex
@@ -1,2 +1,5 @@
 defmodule CensoredStrings do
+  def uncensor(string, _vowels) do
+    string
+  end
 end
--- test/censored_strings_test.exs
+++ test/censored_strings_test.exs
@@ -1,4 +1,26 @@
 defmodule CensoredStringsTest do
   use ExUnit.Case
-  doctest CensoredStrings
+  import CensoredStrings
+
+  describe "CensoredStrings.uncensor/1" do
+    test "replaces the *s with the supplied vowels" do
+      assert uncensor("Wh*r* d*d my v*w*ls g*?", "eeioeo") == "Where did my vowels go?"
+    end
+
+    test "works with no vowels." do
+      assert uncensor("abcd", "") == "abcd"
+    end
+
+    test "works with uppercase" do
+      assert uncensor("*PP*RC*S*", "UEAE") == "UPPERCASE"
+    end
+
+    test "works with * at the end" do
+      assert uncensor("Ch**s*, Gr*mm*t -- ch**s*", "eeeoieee") == "Cheese, Grommit -- cheese"
+    end
+
+    test "Works with * at the start" do
+      assert uncensor("*l*ph*nt", "Eea") == "Elephant"
+    end
+  end
 end
