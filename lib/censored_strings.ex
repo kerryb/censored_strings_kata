4f5cf8a680bc4204a4a7571389e88ee9d8eeaa92
Kerry Buckley <kerryjbuckley@gmail.com>
2020-06-26 10:44:24 +0100

    Oh, that was easy 😁

--- lib/censored_strings.ex
+++ lib/censored_strings.ex
@@ -1,5 +1,11 @@
 defmodule CensoredStrings do
-  def uncensor(string, _vowels) do
-    string
+  def uncensor(string, vowels) do
+    vowels
+    |> String.codepoints()
+    |> Enum.reduce(string, &replace_asterisk/2)
+  end
+
+  defp replace_asterisk(vowel, string) do
+    String.replace(string, "*", vowel, global: false)
   end
 end
